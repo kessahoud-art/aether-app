337df1ef453e647dc2f604a9c128f108b62a03fd
kessahoud-art <kessahoud@gmail.com>
2026-05-10 23:27:54 +0100

    Delete api/auth/callback.js

--- api/auth/callback.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { code } = req.query
-  if (code) {
-    // Exchange code → session (pour OAuth futur)
-    res.redirect('/flux')
-  } else {
-    res.redirect('/login')
-  }
-}
